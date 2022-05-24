ad66f2763405a54bffcc6e82ef3010ebf3a2980f
FelipeFerrazDM <95928734+FelipeFerrazDM@users.noreply.github.com>
2022-05-24 17:10:50 -0400

    ajuste 1

--- src/app/public/js/referente.js
+++ src/app/public/js/referente.js
@@ -1,35 +1,8 @@
-var referente = document.getElementsByClassName("referente");
-var ponte = document.getElementsByClassName("ponte");
-function localizar(index) {
-    switch (index) {
-        case 1:
-            
-            ponte[index].focus()
+const $doc = $('html, body');
 
-            break;
-        case 1.1:
-            console.log("apertou " + index);
-            break;
-        case 1.2:
-            console.log("apertou " + index);
-            break;
-        case 1.3:
-            console.log("apertou " + index);
-            break;
-        case 2:
-            console.log("apertou " + index);
-            break;
-        case 2.1:
-            console.log("apertou " + index);
-            break;
-        case 2.2:
-            console.log("apertou " + index);
-            break;
-        case 2.3:
-            console.log("apertou " + index);
-            break;
-        case 2.4:
-            console.log("apertou " + index);
-            break;
-    }
-}
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 2000);
+    return false;
+});
